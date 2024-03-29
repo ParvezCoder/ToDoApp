9214013d070db41943293804adb0e5d0ec01d1aa
parvez <parvezcoder786@gmail.com>
2024-03-30 04:36:31 +0500

    First Comments

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+//Parvez Coder
+import inquirer from "inquirer";
+let todo =[];
+let condition=true;
+while (condition)
+
+
+{
+let addTask =await inquirer.prompt([{
+name:"todo",
+type:"input",
+message:"What you want  to add your Something ???"
+
+},
+{
+    name:"adMore",
+    type:"confirm",
+    message:"Do you want to add more?????",
+    default:false,
+}
+
+]);
+todo.push(addTask.todo)
+
+condition=addTask.adMore;
+console.log(todo);
+}
